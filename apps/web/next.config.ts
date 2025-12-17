8d4743bfcffe70de38830a934e26ac0fa51344b9
two678 <two6788@gmail.com>
2025-12-17 16:38:27 +0900

    feat: 장소 정보카드 구현

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['@together-dog/ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
