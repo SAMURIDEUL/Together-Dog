7fb22027bf14cf97f0cf7be099274ace9207151e
two678 <two6788@gmail.com>
2025-12-23 16:19:34 +0900

    refactor: 번들 수정

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'], // 진입점
+  entry: ['src/**/*.{ts,tsx}'], // 모든 소스 파일 개별 변환
+  bundle: false, // 번들링 하지 않음 (Tree Shaking 지원)
+  splitting: false,
   format: ['esm', 'cjs'], // ES module + CommonJS 빌드
   dts: true, // 타입 선언 파일 (.d.ts) 생성
   sourcemap: true, // 디버깅용 소스맵
