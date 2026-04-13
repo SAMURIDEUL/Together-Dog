7a81e6be25860493a3fccfe7bad308f6e37edc34
two678 <two6788@gmail.com>
2026-04-13 19:08:23 +0900

    feat: 상세 페이지 주차 정보 추가

--- apps/web/src/app/places/[id]/components/info/BasicInfoSection.tsx
+++ apps/web/src/app/places/[id]/components/info/BasicInfoSection.tsx
@@ -41,6 +41,13 @@ export const BasicInfoSection = ({ place }: BasicInfoSectionProps) => {
           <span className='w-16 shrink-0 text-gray-500'>전화번호</span>
           <span className='text-gray-900'>{place.phone || '정보 없음'}</span>
         </div>
+
+        <div className='flex items-center gap-3'>
+          <span className='w-16 shrink-0 text-gray-500'>주차</span>
+          <span className='text-gray-900'>
+            {place.parkingAvailable ? '가능' : '불가'}
+          </span>
+        </div>
       </div>
     </>
   );
